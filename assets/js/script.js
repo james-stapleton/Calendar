27172f17c9cc3fc75dbcf7f78d377c31d6b51286
“jms” <jstapleton536@gmail.com>
2022-08-25 16:47:04 -0400

    added icons to buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,6 +23,15 @@ $(".saveBtn").on("click", function() {
     $("#11 .description").val(localStorage.getItem("11"));
     $("#12 .description").val(localStorage.getItem("12"));
     $("#13 .description").val(localStorage.getItem("13"));
+    $("#14 .description").val(localStorage.getItem("14"));
+    $("#15 .description").val(localStorage.getItem("15"));
+    $("#16 .description").val(localStorage.getItem("16"));
+    $("#17 .description").val(localStorage.getItem("17"));
+    $("#18 .description").val(localStorage.getItem("18"));
+    $("#19 .description").val(localStorage.getItem("19"));
+    $("#20 .description").val(localStorage.getItem("20"));
+    $("#21 .description").val(localStorage.getItem("21"));
+    $("#22 .description").val(localStorage.getItem("22"));
 
     
 $(".colorcode").each(function() {
