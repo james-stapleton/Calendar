f7e5b4ba056189b0e6fbd5fa356a17401c302dbd
James Stapleton <jstapleton536@gmail.com>
2022-08-25 16:37:01 -0400

    Reload entries using local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,7 +15,28 @@ $(".saveBtn").on("click", function() {
 
 });
 
-for (let index = 0; index < array.length; index++) {
-    const element = array[index];
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];}
+
     $("#9 .description").val(localStorage.getItem("9"));
-}
+    $("#10 .description").val(localStorage.getItem("10"));
+    $("#11 .description").val(localStorage.getItem("11"));
+    $("#12 .description").val(localStorage.getItem("12"));
+    $("#13 .description").val(localStorage.getItem("13"));
+
+    
+$(".colorcode").each(function() {
+    var now = moment().hours();
+    console.log(now);
+    if($(this).attr("id") == now) {
+        $(this).addClass("present");
+    }
+    if($(this).attr("id") > now) {
+        $(this).addClass("future");
+    }
+    else {
+        $(this).addClass("past");
+
+    }
+
+});
